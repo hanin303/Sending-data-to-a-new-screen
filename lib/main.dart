3b8cb85034b1e8a1175431cbc699e9c42f97a4ec
hanin303 <benjemaahanin@gmail.com>
2021-11-25 00:56:34 +0100

    Display the list of todos using a ListView

--- lib/main.dart
+++ lib/main.dart
@@ -12,3 +12,11 @@ final todos = List.generate(
     'A description of what needs to be done for Todo $i',
   ),
 );
+ListView.builder(
+itemCount: todos.length,
+itemBuilder: (context, index) {
+return ListTile(
+title: Text(todos[index].title),
+);
+},
+);
