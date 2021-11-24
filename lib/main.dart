c90113c10f9cce18015ed67c1f073eb85b3a9d94
hanin303 <benjemaahanin@gmail.com>
2021-11-25 00:54:28 +0100

    Define a todo class

--- lib/main.dart
+++ lib/main.dart
@@ -68,3 +68,9 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+class Todo {
+  final String title;
+  final String description;
+
+  const Todo(this.title, this.description);
+}
